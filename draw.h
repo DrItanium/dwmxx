c8304db53618e9d5fbe15cec934e0c4865072f3a
Josh Scoggins <jscoggins@parasoft.com>
2019-04-08 16:36:21 -0700

    Fix up more of the factored libs

--- draw.h
+++ draw.h
@@ -25,15 +25,19 @@ namespace Fontconfig
     using Pattern = ::FcPattern;
 } // end namespace Fontconfig
 namespace dwm {
-enum class ColorSchemeIndex
+enum class ColorschemeIndex
 {
     Foreground,
     Background,
     Border,
 };
 using Color = Xft::Color;
+struct Draw;
 struct Cursor {
     X::Cursor _cursor;
+    Draw& _d;
+    Cursor(Draw& d, int shape);
+    ~Cursor();
 };
 
 class Font {
@@ -43,6 +47,7 @@ class Font {
     auto getH() const noexcept { return _h; }
     auto getXFont() const noexcept { return _xfont; }
     auto getPattern() const noexcept { return _pattern; }
+    void getExts(const std::string& text, unsigned int len, unsigned int* w, unsigned int* h);
 private:
     X::Display& _display;
     unsigned int _h;
@@ -55,24 +60,35 @@ public:
     using FontList = std::list<Font>;
     using FontIterator = FontList::iterator;
     using ConstFontIterator = FontList::const_iterator;
+    using Colorscheme = std::list<Color>;
+    using ColorschemeIterator= Colorscheme::iterator;
+    using ConstColorschemeIterator = Colorscheme::const_iterator;
 public:
     Draw(X::Display* display, int screen, X::Window win, unsigned int w, unsigned int h);
     ~Draw();
     void resize(unsigned int w, unsigned int h);
+    unsigned int getFontWidth(const std::string& name) const noexcept;
+
+public: // extra helpers
     auto getW() const noexcept { return _w; }
     auto getH() const noexcept { return _h; }
     auto getScreen() const noexcept { return _screen; }
     X::Window& getRoot() noexcept { return _root; }
     X::Drawable& getDrawable() noexcept { return _drawable; }
     X::GC& getGC() noexcept { return _gc; }
-    auto begin() noexcept { return _fonts.begin(); }
-    auto begin() const noexcept { return _fonts.begin(); }
-    auto cbegin() const noexcept { return _fonts.cbegin(); }
-    auto end() noexcept { return _fonts.end(); }
-    auto end() const noexcept { return _fonts.end(); }
-    auto cend() const noexcept { return _fonts.cend(); }
+public: // font set related operations
+    auto font_begin() noexcept { return _fonts.begin(); }
+    auto font_begin() const noexcept { return _fonts.begin(); }
+    auto font_cbegin() const noexcept { return _fonts.cbegin(); }
+    auto font_end() noexcept { return _fonts.end(); }
+    auto font_end() const noexcept { return _fonts.end(); }
+    auto font_cend() const noexcept { return _fonts.cend(); }
     void installFont(const std::string& font) noexcept;
     void installFont(const std::initializer_list<std::string>& fonts) noexcept;
+public: // colorscheme related
+    
+public: // cursor related
+
 private:
     unsigned int _w, _h;
     X::Display& _dpy;
