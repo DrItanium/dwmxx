83fdb6850302ff1caa4e92485919f3ad72c7fcfa
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-04-08 18:55:55 -0700

    Start writing dwm.cc

---
+++ dwm.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cstdint>
+#include <errno.h>
+#include <locale.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <X11/cursorfont.h>
+#include <X11/keysym.h>
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+#include <X11/Xproto.h>
+#include <X11/Xutil.h>
+#ifdef XINERAMA
+#include <X11/extensions/Xinerama.h>
+#endif /* XINERAMA */
+#include <X11/Xft/Xft.h>
+
+namespace dwm 
+{
+//    constexpr auto ButtonMask = ButtonPressMask | ButtonReleaseMask;
+    //template<typename T>
+    //constexpr T cleanMask(T mask) noexcept {
+    //    return (mask & ~(numlockmask|LockMas
+    //}
+}
